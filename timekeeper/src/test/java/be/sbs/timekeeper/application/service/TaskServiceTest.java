9d01f672d40aa43e21985be5913102be1595e492
Fahmei Tahiri <fahmei.tahiri@tvh.com>
2018-12-01 20:31:05 +0100

    Solved eclipse Junit 5 compatibility maven/select maven profiles/eclipse

--- timekeeper/src/test/java/be/sbs/timekeeper/application/service/TaskServiceTest.java
+++ timekeeper/src/test/java/be/sbs/timekeeper/application/service/TaskServiceTest.java
@@ -1,17 +1,8 @@
 package be.sbs.timekeeper.application.service;
 
 
-import be.sbs.timekeeper.application.beans.Project;
-import be.sbs.timekeeper.application.exception.BadRequestException;
-import be.sbs.timekeeper.application.repository.ProjectRepository;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class TaskServiceTest {
