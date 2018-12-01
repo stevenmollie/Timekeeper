ce7fe8c317c74ebca353b24008fa1bb2aff3d9ac
Fahmei Tahiri <fahmei.tahiri@tvh.com>
2018-12-01 15:09:57 +0100

    moved Project service testing to the right class

--- timekeeper/src/test/java/be/sbs/timekeeper/application/service/TaskServiceTest.java
+++ timekeeper/src/test/java/be/sbs/timekeeper/application/service/TaskServiceTest.java
@@ -16,38 +16,5 @@ import static org.mockito.Mockito.verify;
 @ExtendWith(MockitoExtension.class)
 class TaskServiceTest {
 
-    @Mock
-    private ProjectRepository projectRepository;
 
-    @InjectMocks
-    private ProjectService projectService;
-
-    @Captor
-    private ArgumentCaptor<Project> projectArgumentCaptor;
-
-    static final String PROJECT_ID = "123456abc";
-
-    @Test
-    void test_addProject() {
-        Project project = createProject(PROJECT_ID, "project", "");
-        projectService.addProject(project);
-        verify(projectRepository).insert(projectArgumentCaptor.capture());
-        Project result = projectArgumentCaptor.getValue();
-        assertThat(project).isEqualToComparingFieldByField(result);
-    }
-
-
-    @Test
-    void test_addProject_throwsExceptionWhenNameIsEmpty() {
-        assertThrows(BadRequestException.class,
-                () -> projectService.addProject(createProject(PROJECT_ID, null, "some description")));
-    }
-
-    private Project createProject(String id, String name, String description) {
-        Project project = new Project();
-        project.setId(id);
-        project.setName(name);
-        project.setDescription(description);
-        return project;
-    }
 }
